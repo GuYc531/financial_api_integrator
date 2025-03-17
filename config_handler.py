63af3b9846cad501194a75f0ab75207011a8caae
guy <gguy531997@gmail.com>
2025-03-17 17:55:56 +0200

    added env vars validation logs

--- config_handler.py
+++ config_handler.py
@@ -1,4 +1,5 @@
 import os
+from types import NoneType
 
 from logger import Logger
 import logging
@@ -7,6 +8,8 @@ class Configs:
     def __init__(self):
         self.log = Logger(name=__name__, log_file="logs/app.log", level=logging.DEBUG).get_logger()
         self.api_key = os.getenv('POLYGON_API_KEY')
+        self.latest = False if int(os.getenv('latest')) == 0 else True
+
         self.date_column_name = os.getenv('date_column_name')
         self.ticker = os.getenv('ticker')
         self.date_to_fetch_from = os.getenv('date_to_fetch_from')
@@ -19,5 +22,10 @@ class Configs:
         self.base_currency = os.getenv('base_currency')
         self.currency_to_convert_to = os.getenv('currency_to_convert_to')
         self.stock_price_column_to_convert = [i for i in list(os.getenv('stock_price_column_to_convert')) if i != ',']
-        self.latest = False if int(os.getenv('latest')) == 0 else True
+        self._validate_environment_variables()
         self.log.info("all env vars initialized correctly")
+
+    def _validate_environment_variables(self):
+        for (env_var_key, env_var_val) in self.__dict__.items():
+            if isinstance(env_var_val, NoneType):
+                self.log.error(f"Notice, user did not initialize environment variable {env_var_key}")
